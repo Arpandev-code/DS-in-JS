c5c8033c3f8b6f34e0f80a4b68d411a5e06127d5
Arpandev-code <arpan.dev2016@gmail.com>
2024-07-29 20:07:15 +0530

    Update BST

--- BinarySearchTree/bst.js
+++ BinarySearchTree/bst.js
@@ -43,4 +43,48 @@ class BinarySearchTree{
         }
 
     }
+    //deletion operation
+    delete(key)
+    {
+        this.root= this.deleteNode(this.root,key)
+    }
+    deleteNode(node,key)// node-> root node
+    {
+        if(node===null)
+        {
+            return null
+        }
+
+        if(key<node.key)
+        {
+            node.left=this.deleteNode(node.left,key)
+        }else if(key>node.key)
+        {
+            node.right=this.deleteNode(node.right,key)
+        }else{
+            if(node.right===null && node.left===null)
+            {
+                return null;
+            }else if(node.right===null)
+            {
+                return node.left
+            }else if(node.lelf===null)
+            {
+                return node.right;
+            }else{
+                let tempNode=this.findMinNode(node.right)
+                node.key=tempNode.key;
+                node.right=this.deleteNode(node.right,tempNode.key)
+            }
+        }
+        return node;
+    }
+    findMinNode(node)
+    {
+        while(node!==null)
+        {
+            node=node.right
+        }
+        return node
+    }
 }
